8d1f321e3defce255a594b3f44e9cdbbb792fafd
unknown <junuibag629-sudo>
2026-07-11 13:48:39 +0900

    test.py 코드 수정함_2

--- test.py
+++ test.py
@@ -2,11 +2,12 @@ korean, english, mathematics, science = map(int, input('국어 영어 수학 과
 
 def get_min_max_score(*args):
     return min(args), max(args)
-def get_average(*args):
-    return average(args)
+def get_average(**args):
+    return sum(args.values())/len(args)
+    
     
 min_score, max_score = get_min_max_score(korean, english, mathematics, science)
-average_score = get_average(korean=korean, english=english, mathematics=mathematics, science=science)
+average_score = get_average(korean=korean, english=english, mathematics=mathematics, science=science) 
 print('낮은 점수: {0:.2f}, 높은 점수: {1:.2f}, 평균 점수: {2:.2f}'.format(min_score, max_score, average_score))
 
 min_score, max_score = get_min_max_score(english, science)
